fa9348c3b8ce05d31504180cb8b6625dbcab0aff
Hugo Lopes <hugo.rodrigue.lopes@gmail.com>
2023-09-16 13:50:23 +0100

    fix auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,17 +1,7 @@
 import NextAuth from 'next-auth';
-import DiscordProvider from 'next-auth/providers/discord';
 
-const authOptions = {
-  // Configure one or more authentication providers
-  providers: [
-    DiscordProvider({
-      clientId: process.env.DISCORD_CLIENT_ID ?? '',
-      clientSecret: process.env.DISCORD_CLIENT_SECRET ?? '',
-    }),
-    // ...add more providers here
-  ],
-};
+import authOptions from './autOptions';
 
 const handler = NextAuth(authOptions);
 
-export { authOptions, handler as GET, handler, handler as POST };
+export { handler as GET, handler as POST };
