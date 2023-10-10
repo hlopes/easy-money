0b45c7b55b151fb7f4bd813271c169efc2fcb3f5
Hugo Lopes <hugo.rodrigue.lopes@gmail.com>
2023-10-10 21:40:14 +0100

    chore: some cleanup

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,60 +8,15 @@ module.exports = {
     '@typescript-eslint',
     'import',
     'react',
-    'jsx-a11y',
     'react-hooks',
+    'jsx-a11y',
     'simple-import-sort',
   ],
-  ignorePatterns: [
-    '!.*',
-    'build',
-    'coverage',
-    'node_modules',
-    'public',
-    'storybook-static',
-    '.lintstagedrc.js',
-    'commitlint.config.js',
-    '.eslintrc.js',
-    'prisma',
-  ],
   rules: {
-    'simple-import-sort/imports': [
-      'error',
-      {
-        groups: [
-          // Node builtin modules:
-          ['^node:\\w'],
-          // Third party packages, with React and Next first:
-          ['^react', '^next', '^\\w', '^@?\\w'],
-          ['^@/\\w'],
-          // Parent imports, with `..` first.
-          ['^\\.\\./?$', '^\\.\\.(?!/?$)'],
-          // Other relative imports with nested folders imports first and same folder imports last.
-          ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
-          // Side effect imports.
-          ['^\\u0000'],
-        ],
-      },
-    ],
-    'simple-import-sort/exports': 'error',
     'import/group-exports': 'error',
     'import/no-anonymous-default-export': 'error',
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'error',
-    'no-console': 'error',
-    'no-var': 'error',
-    'no-unused-vars': 'off',
-    'max-len': [
-      'error',
-      {
-        code: 100,
-        ignoreUrls: true,
-        ignoreStrings: true,
-        ignoreRegExpLiterals: true,
-        ignoreTemplateLiterals: true,
-        ignoreComments: true,
-      },
-    ],
+    'jsx-a11y/alt-text': 'error',
+    'jsx-a11y/label-has-associated-control': 'error',
     'padding-line-between-statements': [
       'error',
       {
@@ -90,6 +45,41 @@ module.exports = {
         next: 'default',
       },
     ],
+    'max-len': [
+      'error',
+      {
+        code: 100,
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreRegExpLiterals: true,
+        ignoreTemplateLiterals: true,
+        ignoreComments: true,
+      },
+    ],
+    'no-console': 'error',
+    'no-var': 'error',
+    'no-unused-vars': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'error',
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          // Node builtin modules:
+          ['^node:\\w'],
+          // Third party packages, with React and Next first:
+          ['^react', '^next', '^\\w', '^@?\\w'],
+          ['^@/\\w'],
+          // Parent imports, with `..` first.
+          ['^\\.\\./?$', '^\\.\\.(?!/?$)'],
+          // Other relative imports with nested folders imports first and same folder imports last.
+          ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+          // Side effect imports.
+          ['^\\u0000'],
+        ],
+      },
+    ],
+    'simple-import-sort/exports': 'error',
     '@typescript-eslint/consistent-type-imports': 'error',
     '@typescript-eslint/consistent-type-exports': [
       'error',
@@ -107,4 +97,15 @@ module.exports = {
       },
     ],
   },
+  ignorePatterns: [
+    '!.*',
+    'build',
+    'coverage',
+    'node_modules',
+    'public',
+    'prisma',
+    'commitlint.config.js',
+    '.eslintrc.js',
+    'postcss.config.js',
+  ],
 };
